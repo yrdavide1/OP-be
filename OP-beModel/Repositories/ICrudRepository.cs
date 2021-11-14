ebe0fdaae90780c92e1ace8ee4e5f61bff874786
yrdavide1 <davicatra@gmail.com>
2021-11-14 20:18:04 +0100

    icrudrepo

---
+++ OP-beModel/Repositories/ICrudRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OP_beModel.Repositories
+{
+    public interface ICrudRepository<T, K>
+    {
+        T Create(T entity);
+        void Delete(T entity);
+        void Delete(K key);
+        T Update(T entity);
+        T FindById(K key);
+        IEnumerable<T> GetAll();
+    }
+}
