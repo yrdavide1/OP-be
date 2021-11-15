ce3fc5f7f3e8bdff5d3feba2a66877f4e05f0f3a
Davide Catrambone <davide.catrambone@runtime.local>
2021-11-16 00:52:40 +0100

    FIRST USER API WORKING

--- OP-be/Program.cs
+++ OP-be/Program.cs
@@ -1,7 +1,24 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using OP_beContext.EFContext;
+using OP_beContext.Repositories;
+using OP_beContext.Services;
+using OP_beModel.Repositories;
+using OP_beModel.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = "Server = 173.249.39.182; User Id=SA; Password=9EFD8a1744@; Database = OP";
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<OPbeContext>(x => x.UseSqlServer(connectionString));
+builder.Services.AddScoped<IUserRepository, EFUserRepository>();
+builder.Services.AddScoped<IPeopleService, EFPeopleService>();
+builder.Services.AddCors(c => c.AddPolicy("alloworigin", options => options.AllowAnyOrigin()
+                                                                            .AllowAnyHeader()
+                                                                            .AllowAnyMethod()));
+//AutoMapper config
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
