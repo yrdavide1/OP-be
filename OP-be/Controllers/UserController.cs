31be4f384e3594ee46efafd1a2140a7fa60202df
Davide Catrambone <davide.catrambone@runtime.local>
2021-11-15 18:03:40 +0100

    added controller (not working atm)

---
+++ OP-be/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using OP_be.DTOs;
+using OP_beModel.Services;
+
+namespace OP_be.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private IPeopleService service;
+        private IMapper mapper;
+
+        public UserController(IPeopleService service, IMapper mapper)
+        {
+            this.service = service;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = service.GetAll();
+            var usersDTOs = mapper.Map<IEnumerable<UserDTO>>(users);
+            return Ok(usersDTOs);
+        }
+    }
+}
---
+++ OP-be/DTOs/UserDTO.cs
@@ -0,0 +1,36 @@
+﻿using OP_beModel.Entities;
+
+namespace OP_be.DTOs
+{
+    public class UserDTO
+    {
+        public long PersonId { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? FullName { get; set; }
+        public string? DateOfBirth { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public string? Gender { get; set; }
+        public bool? IsUser { get; set; }
+
+        public UserDTO() { }
+
+        public UserDTO(User original)
+        {
+            PersonId = original.PersonId;
+            Firstname = original.Firstname;
+            Lastname = original.Lastname;
+            FullName = original.FullName;
+            Gender = original.Gender;
+            PhoneNumber = original.PhoneNumber;
+            DateOfBirth = original.DateOfBirth;
+            Address = original.Address;
+            City = original.City;
+            Email = original.Email;
+            IsUser = original.IsUser;
+        }
+    }
+}
